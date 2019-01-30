8d98fae47c7f8bd017617ddb150c154617505e0c
Jakob Dunning <jakob_dunning@duckdesigns.de>
2019-01-31 00:41:19 +0100

    Wrote first functional test

--- tests/EventApiTest.php
+++ tests/EventApiTest.php
@@ -23,4 +23,54 @@ class EventApiTest extends WebTestCase
         
         $this->assertSame(200, $this->client->getResponse()->getStatusCode());
     }
+
+    public function testFetchReturns200()
+    {
+        $postData = '{"title":"banane"}';
+        $this->client->request('POST', '/events', [], [], [], $postData);
+        $location = $this->client->getResponse()->headers->get('Location');
+        $this->client->request('GET', $location);
+        
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+    
+    public function testFetchReturns404()
+    {
+        $path = '/events/999';
+        $this->client->request('DELETE', $path);
+        $this->client->request('GET', $path);
+        
+        $this->assertSame(404, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateReturns201()
+    {
+        $this->client->request('POST', '/events', [], [], [], '{"title":"banane"}');
+        
+        $this->assertSame(201, $this->client->getResponse()->getStatusCode());
+    }
+    
+    public function testCreateReturns500OnBadData()
+    {
+        $this->client->request('POST', '/events', [], [], [], '{}');
+        
+        $this->assertSame(500, $this->client->getResponse()->getStatusCode());
+    }
+    
+    public function testUpdate()
+    {
+        $postData = '{"title":"banane"}';
+        $this->client->request('POST', '/events', [], [], [], $postData);
+        $location = $this->client->getResponse()->headers->get('Location');
+        $this->client->request('PUT', $location, [], [], [], '{"title":"banane"}');
+        
+        $this->assertSame(204, $this->client->getResponse()->getStatusCode());
+    }
+    
+    public function testDelete()
+    {
+        $this->client->request('DELETE', '/events/3');
+        
+        $this->assertSame(204, $this->client->getResponse()->getStatusCode());
+    }
 }
